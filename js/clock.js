8f8b27b82668dd0b8df8c882ae2dbfa30e9a545e
timothy <timothyxvo@gmail.com>
2015-03-05 16:28:05 -0800

    oauth 2.0

--- js/clock.js
+++ js/clock.js
@@ -5,5 +5,10 @@ function getTime() {
 }
 
 window.onload = function() {
-   getTime();
+  
+
+   var obj = {"client_id":"1133a9f7c0025e5", 
+   "client_type":"token", 
+   "callback_function":"https://api.imgur.com/3/account/me/" };
+   init(obj);
 };
