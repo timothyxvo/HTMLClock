66e20f1d4820e4c285f4ada23278937b355510ba
timothy <timothyxvo@gmail.com>
2015-03-05 17:20:09 -0800

    init json

--- js/clock.js
+++ js/clock.js
@@ -5,10 +5,9 @@ function getTime() {
 }
 
 window.onload = function() {
-  
 
    var obj = {"client_id":"1133a9f7c0025e5", 
-   "client_type":"token", 
+   "type_response": "token", 
    "callback_function":"https://api.imgur.com/3/account/me/" };
    init(obj);
 };
